d2b5eca92ea3ff4e7529b50d590b73d87194524c
Olvis Quintana <olvisdevalencia@gmail.com>
2017-11-24 17:08:02 -0300

    only test porpuses

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const onMessage = ({sender, intent, entities, text, reply}) => {
             reply([{text: `¡Hola ${sender.first_name}!`}]);
             break;
         case 'probando':
-        	reply([{text:'Hola como estas , en que te puedo ayudar ?'}]);
+        	reply([{text:'${sender.first_name} ${sender.last_name}: ${text}, en que te puedo ayudar ?'}]);
         	break;
         // The bot didn't understand
         case UNK:
